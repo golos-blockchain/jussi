3c13bd528a3ebd405090baba74d74bfa279beb01
isteric-senior <halfyearforwhat@gmail.com>
2020-09-29 19:24:08 +0300

    Fix Docker build, update Pipfile dependencies #1

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ export IMAGE_TAG=`git ls-remote --heads origin | grep $(git rev-parse HEAD) | cu
 if [ "$IMAGE_TAG" = "master" ] ; then export IMAGE_TAG=latest ; fi
 export REPO_PATH=`git rev-parse --show-toplevel`
 export REPO_NAME=`basename $REPO_PATH`
-export IMAGE_REPO_NAME="steemit/$REPO_NAME"
+export IMAGE_REPO_NAME="golos/$REPO_NAME"
 export SOURCE_COMMIT=`git rev-parse HEAD`
 echo Building branch $IMAGE_TAG from $IMAGE_REPO_NAME
 docker build . -t $IMAGE_REPO_NAME:$IMAGE_TAG --build-arg SOURCE_COMMIT="${SOURCE_COMMIT}" --build-arg DOCKER_TAG="${IMAGE_TAG}"
